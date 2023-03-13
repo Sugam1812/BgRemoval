06e3c742bbeed2bb31796387b0249608486fbe95
Sugam1812 <107319710+Sugam1812@users.noreply.github.com>
2023-03-13 13:21:42 +0530

    Update bg_remove.py

--- bg_remove.py
+++ bg_remove.py
@@ -1,39 +1,42 @@
 import streamlit as st
 from rembg import remove
 from PIL import Image
-import io
+from io import BytesIO
+import base64
 
-# Set page configuration
-st.set_page_config(page_title='Background Remover', page_icon=':camera:', layout='wide')
+st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-# Set page title and description
-st.write('# Background Remover')
-st.write('Remove the background from your images!')
+st.write("## Remove background from your image")
+st.write(
+    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+)
+st.sidebar.write("## Upload and download :gear:")
 
-# Create a file uploader widget
-uploaded_file = st.file_uploader('Choose an image file', type=['jpg', 'jpeg', 'png'])
 
-# Define a function to remove background from the image
-def remove_background(image):
-    # Load the image
-    img = Image.open(io.BytesIO(image))
+# Download the fixed image
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
 
-    # Remove the background
-    result = remove(img)
 
-    # Return the result as bytes
-    buffered = io.BytesIO()
-    result.save(buffered, format='PNG')
-    return buffered.getvalue()
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Original Image :camera:")
+    col1.image(image)
 
-# Display the uploaded image and the result
-if uploaded_file is not None:
-    # Remove the background from the uploaded image
-    image_bytes = uploaded_file.read()
-    result_bytes = remove_background(image_bytes)
+    fixed = remove(image)
+    col2.write("Fixed Image :wrench:")
+    col2.image(fixed)
+    st.sidebar.markdown("\n")
+    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
 
-    # Display the images side by side
-    st.image([Image.open(io.BytesIO(image_bytes)), Image.open(io.BytesIO(result_bytes))], 
-             caption=['Original Image', 'Background Removed'], width=300)
+
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if my_upload is not None:
+    fix_image(upload=my_upload)
 else:
-    st.write('Upload an image to get started!')
+    fix_image("./gops.jpeg")
